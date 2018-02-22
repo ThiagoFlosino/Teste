66da5cbb8612441701daec4736f28b681d3a64f1
Thiago Flosino <thiagoflosino@gmail.com>
2018-02-22 17:52:03 -0300

    Atualizando

--- src/main/java/com/atislabs/buscador/configuration/Configuration.java
+++ src/main/java/com/atislabs/buscador/configuration/Configuration.java
@@ -1,64 +1,14 @@
 package com.atislabs.buscador.configuration;
 
-import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-<<<<<<< HEAD
 
 @EnableTransactionManagement
 @ComponentScan("com.atislabs.buscador")
 @EnableNeo4jRepositories("com.atislabs.buscador.repository")
-@org.springframework.context.annotation.Configuration
 public class Configuration {
-=======
->>>>>>> branch 'master' of https://github.com/ThiagoFlosino/Teste.git
 
-
-<<<<<<< HEAD
-
-//	  @Bean
-//	  public SessionFactory getSessionFactory() {
-//	    return new SessionFactory(configuration(), "com.atislabs.buscador.domain");
-//	  }
-//
-//	  @Bean
-//	  public Neo4jTransactionManager transactionManager() throws Exception {
-//	    return new Neo4jTransactionManager(getSessionFactory());
-//	  }
-//
-//	  @Bean
-//	  public org.neo4j.ogm.config.Configuration configuration() {
-//	    return new Builder()
-//	      .uri("bolt://localhost")
-//	      .build();
-//	  }
-=======
-@EnableTransactionManagement
-@ComponentScan("com.atislabs.buscador")
-@EnableNeo4jRepositories("com.atislabs.buscador.repository")
-public class Configuration {
-/*
-	@Bean
-	  public SessionFactory getSessionFactory() {
-	    return new SessionFactory(configuration(), "org.neo4j.cineasts.domain");
-	  }
-
-	  @Bean
-	  public Neo4jTransactionManager transactionManager() throws Exception {
-	    return new Neo4jTransactionManager(getSessionFactory());
-	  }
-
-	  @Bean
-	  public org.neo4j.ogm.config.Configuration configuration() {
-	    return new org.neo4j.ogm.config.Configuration.Builder()
-	      .uri("bolt://localhost")
-	      .build();
-	  }*/
-	
->>>>>>> branch 'master' of https://github.com/ThiagoFlosino/Teste.git
 }
 
 	
